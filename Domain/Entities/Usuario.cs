624b8add49d478fc06e212e086264f63e985321b
joaoMoraes-git <joaoguiilhermee06@gmail.com>
2025-11-25 00:52:08 -0300

    Entities meio feitas, faltando FluentAPI

--- Domain/Entities/Usuario.cs
+++ Domain/Entities/Usuario.cs
@@ -2,20 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace api_usuarios_as_João_Guilherme.domain
 {
     public class Usuario
     {
-        public int Id { get; set; } //PK, Auto-increment - X
-        public string Nome { get; set; } = string.Empty; //Obrigatório, 3-100 caracteres - X
-        public string Email { get; set; } = string.Empty; //Obrigatório, formato válido, único - X
-        public string Senha { get; set; } = string.Empty; //Obrigatório, min 6 caracteres - X
-        public DateTime DataNascimento { get; set; } //Obrigatório, idade >= 18 anos - X
-        public string? Telefone { get; set; } //Opcional, formato (XX) XXXXX-XXXX - X
-        public bool Ativo { get; set; } //Obrigatório, default true - X
-        public DateTime DataCriacao { get; set; } //Obrigatório, preenchido automaticamente - X 
-        public DateTime? DataAtualizacao { get; set; } // Opcional, atualizacao automaticamente - x
+        [Key]
+        //Auto-increment aqui
+        public int Id { get; set; } //PK, Auto-increment - ...
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Nome { get; set; } = string.Empty; //Obrigatório, 3-100 caracteres - V
+
+        [Required]
+        [EmailAddress]
+        // Único / FluentAPI aqui
+        public string Email { get; set; } = string.Empty; //Obrigatório, formato válido, único - ... [FluentAPI]
+
+        [Required]
+        [MinLength(6)]
+        public string Senha { get; set; } = string.Empty; //Obrigatório, min 6 caracteres - V
+
+        [Required]
+        // Verificação de idade / FluentAPI aqui
+        public DateTime DataNascimento { get; set; } //Obrigatório, idade >= 18 anos - ... [FluentAPI]
+
+        // Verificação de formato / FluentAPI aqui
+        public string? Telefone { get; set; } //Opcional, formato (XX) XXXXX-XXXX - X [FluentAPI]
+
+        [Required]
+        // Default / FluentAPI aqui
+        public bool Ativo { get; set; } //Obrigatório, default true - ... [FluentAPI]
+
+        [Required]
+        // Preenchimento automatico / FluentAPI aqui
+        public DateTime DataCriacao { get; set; } = DateTime.Now; //Obrigatório, preenchido automaticamente - X [FluentAPI]
+
+        // Atualização automatica / FluentAPI aqui
+        public DateTime? DataAtualizacao { get; set; } // Opcional, atualizacao automaticamente - x [FluentAPI]
 
     }
 }
