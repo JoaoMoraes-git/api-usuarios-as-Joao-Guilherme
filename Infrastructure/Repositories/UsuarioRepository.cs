7677b2acfe7cf620ad86810a66f2491039341298
joaoMoraes-git <joaoguiilhermee06@gmail.com>
2025-11-27 00:55:02 -0300

    Feat: Repository quase pronto, faltando GetEmailAsync e EmailExistsAsync

--- Infrastructure/Repositories/UsuarioRepository.cs
+++ Infrastructure/Repositories/UsuarioRepository.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using api_usuarios_as_João_Guilherme.domain;
 using api_usuarios_as_João_Guilherme.Application.Interfaces;
 using api_usuarios_as_João_Guilherme.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace api_usuarios_as_João_Guilherme.Infrastructure.Repositories
 {
@@ -15,13 +16,13 @@ namespace api_usuarios_as_João_Guilherme.Infrastructure.Repositories
         //Consulta todas as entidades
         public async Task<IEnumerable<Usuario>> GetAllAsync (CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            return await _context.Usuarios.AsNoTracking().ToListAsync(ct);
         }
 
         //Procura a entidade com o id enviado
         public async Task<Usuario?> GetByIdAsync(int id, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            return await _context.Usuarios.FirstOrDefaultAsync(p => p.Id == id, ct);
         }
 
         //Procura a entidade com o email enviado
@@ -33,19 +34,22 @@ namespace api_usuarios_as_João_Guilherme.Infrastructure.Repositories
         //Adiciona uma nova entidade ao banco de dados
         public async Task AddAsync(Usuario usuario, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            await _context.Usuarios.AddAsync(usuario, ct);
+            // await _context.SaveChangesAsync(ct);
         }
 
         //Atualiza uma entidade do banco de dados
-        public async Task UpdateAsync(Usuario usuario, CancellationToken ct = default)
+        public Task UpdateAsync(Usuario usuario, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            _context.Usuarios.Update(usuario);
+            return Task.CompletedTask;
         }
 
         //Remove uma entidade do banco de dados
         public async Task RemoveAsync(Usuario usuario, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            _context.Remove(usuario);
+            // await _context.SaveChangesAsync(ct);
         }
 
         //Verifica se o email enviado já existe
@@ -57,7 +61,7 @@ namespace api_usuarios_as_João_Guilherme.Infrastructure.Repositories
         //Salva as alterações feitas
         public async Task SaveChangesAsync(CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            await _context.SaveChangesAsync(ct);
         }
     }
 }
